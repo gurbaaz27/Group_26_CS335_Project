8f0c46415c56f718804cbbb8e17b92daf57abe22
dipanshu124 <dipanshu@iitk.ac.in>
2022-01-30 17:31:25 +0530

    Add Regular expression rules for operators and punctuation.

--- src/lexer.py
+++ src/lexer.py
@@ -21,6 +21,96 @@ def columnno(input, token):
 lexer = lex.lex()
 lex.colno = 0
 
+tokens = (
+    #Operators and Punctuation
+    "RIGHT_SHIFT_EQUAL",
+    "LEFT_SHIFT_EQUAL",
+    "RIGHT_SHIFT",
+    "LEFT_SHIFT",
+    "PLUS_ASSIGN",
+    "MINUS_ASSIGN",
+    "MULT_ASSIGN",
+    "DIV_ASSIGN",
+    "MOD_ASSIGN",
+    "AND_ASSIGN",
+    "OR_ASSIGN",
+    "XOR_ASSIGN",
+    "COLON_ASSIGN",
+    "AND",
+    "OR",
+    "INCREMENT",
+    "DECREMENT",
+    "EQUAL",
+    "NOT_EQUAL",
+    "LESS_EQUAL",
+    "GREATER_EQUAL",
+    "PLUS",
+    "MINUS",
+    "MULT",
+    "DIV",
+    "MOD",
+    "BIT_AND",
+    "BIT_OR",
+    "BIT_XOR",
+    "LESS",
+    "GREATER",
+    "ASSIGN",
+    "NOT",
+    "LEFT_PARENTH",
+    "RIGHT_PARENTH",
+    "LEFT_SQUARE",
+    "RIGHT_SQUARE",
+    "LEFT_BRACE",
+    "RIGHT_BRACE",
+    "COMMA",
+    "DOT",
+    "COLON",
+    "SEMICOLON",
+)
+
+t_RIGHT_SHIFT_EQUAL = r">>="
+t_LEFT_SHIFT_EQUAL = r"<<="
+t_RIGHT_SHIFT = r">>"
+t_LEFT_SHIFT = r"<<"
+t_PLUS_ASSIGN = r"\+="
+t_MINUS_ASSIGN = r"-="
+t_MULT_ASSIGN = r"\*="
+t_DIV_ASSIGN = r"/="
+t_MOD_ASSIGN = r"%="
+t_AND_ASSIGN = r"&="
+t_OR_ASSIGN = r"\|="
+t_XOR_ASSIGN = r"\^="
+t_COLON_ASSIGN = r":="
+t_AND = r"&&"
+t_OR = r"\|\|"
+t_INCREMENT = r"\+\+"
+t_DECREMENT = r"--"
+t_EQUAL = r"=="
+t_NOT_EQUAL = r"!="
+t_LESS_EQUAL = r"<="
+t_GREATER_EQUAL = r">="
+t_PLUS = r"\+"
+t_MINUS = r"-"
+t_MULT = r"\*"
+t_DIV = r"/"
+t_MOD = r"%"
+t_BIT_AND = r"&"
+t_BIT_OR = r"\|"
+t_BIT_XOR = r"\^"
+t_LESS = r"<"
+t_GREATER = r">"
+t_ASSIGN = r"="
+t_NOT = "!"
+t_LEFT_PARENTH = r"\("
+t_RIGHT_PARENTH = r"\)"
+t_LEFT_SQUARE = r"\["
+t_RIGHT_SQUARE = r"\]"
+t_LEFT_BRACE = r"\{"
+t_RIGHT_BRACE = r"\}"
+t_COMMA = r","
+t_DOT = r"\."
+t_COLON = r":"
+t_SEMICOLON = r";"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
