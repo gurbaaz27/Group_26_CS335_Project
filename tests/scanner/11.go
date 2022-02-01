6fb5517bb448da436a5af854ef8dd5a01d06e147
dipanshu124 <dipanshu@iitk.ac.in>
2022-02-01 12:00:33 +0530

    Add test cases for lexer

---
+++ tests/scanner/11.go
@@ -0,0 +1,10 @@
+// This is a one line comment.
+/* This is a multiline
+ comment */
+
+//*//* // /*/ /*\*/*///***/*/*/*/*/*/*///***
+
+/*//This is another comment//*/
+
+/* /* /* Check for nesting/* * *** /
+/////*/
---
+++ tests/scanner/12.go
@@ -0,0 +1,8 @@
+s:= "Tests for strings."
+s := " // /* */."
+s := "\n"
+s := " \"quotes\""
+s := "multi
+line."
+
+
