a09398e5b5fe30c277d0a4d86c149bbd27c5c563
gurbaaz <gurbaazsn123@gmail.com>
2022-04-10 19:18:28 +0530

    Refactor code into modules

---
+++ src/classes.py
@@ -0,0 +1,58 @@
+###########################
+## Milestone 5 : CS335A ##
+######################################
+## Submission Date : April 10, 2022 ##
+######################################
+
+__author__ = "Group 26, CS335A"
+__filename__ = "classes.py"
+__description__ = "Helper classes for compiler."
+
+
+from typing import List
+
+
+class Format:
+    """
+    Collection of ANSI escape sequences to format strings
+    """
+
+    success: str = "\033[32m"
+    fail: str = "\033[91m"
+    end: str = "\033[0m"
+    underline: str = "\033[4m"
+
+
+class Node:
+    def __init__(
+        self,
+        name: str = "",
+        val: str = "",
+        line_num: int = 0,
+        type: str = "",
+        children: List = [],
+        array: List = [],
+        func: int = 0,
+        level: int = 0,
+        ast=None,
+        code: List = [],
+        place: str = "",
+        dims: List = [],
+        placelist: List = [],
+    ):
+        self.name = name
+        self.val = val
+        self.type = type
+        self.line_num = line_num
+        self.array = array
+        self.func = func
+        self.ast = ast
+        self.level = level
+        self.code = code
+        self.place = place
+        self.dims = dims
+        self.placelist = placelist
+        if children:
+            self.children = children
+        else:
+            self.children = []
---
+++ src/constants.py
@@ -0,0 +1,15 @@
+###########################
+## Milestone 5 : CS335A ##
+######################################
+## Submission Date : April 10, 2022 ##
+######################################
+
+__author__ = "Group 26, CS335A"
+__filename__ = "constants.py"
+__description__ = "Constants required for compiler."
+
+
+SYMBOL_TABLE_DUMP_FILENAME = "symtab.csv"
+AST_FILENAME = "ast.dot"
+AST_PLOT_FILENAME = "ast_plot.png"
+TOKENS_TO_IGNORE = ["{", "}", "(", ")", ";", "[", "]", ","]
