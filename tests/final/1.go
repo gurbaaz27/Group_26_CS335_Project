3c87f2f37b930c7db40e2217e03fddc61f278c59
gurbaaz <gurbaazsn123@gmail.com>
2022-04-23 20:06:24 +0530

    Add sample testcase

---
+++ tests/final/1.go
@@ -0,0 +1,20 @@
+package main
+
+var b int = 9
+
+// func main() {
+// 	var i int = 5
+
+// 	// if i > 10 {
+// 	// 	i = i + 1
+// 	// } else {
+// 	// 	i = i + 2
+// 	// }
+// 	// b = 10
+// 	// var b int8 = 5
+// 	// b = 11
+// 	// var b bool = true
+// 	// var c float64 = 0.9
+// 	// a = a + 5
+// 	return
+// }
